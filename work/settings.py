552fa6ebfd51825bdb92c09bd8c3ed3645f04c2f
rvfedorin <35657347+rvfedorin@users.noreply.github.com>
2018-10-17 13:42:43 +0300

    add crypt

--- work/settings.py
+++ work/settings.py
@@ -1,4 +1,3 @@
-import os
 
 data_path = '/data/'
 help_file = data_path+'help.txt'
@@ -10,5 +9,10 @@ client_to_multi_vlan = f'{data_path}client_to_multi_vlan.txt'
 city_shelve = f'{data_path}city.shelve'
 # log = '../log/'
 
+iv = b'\x88\xc3p\\\xd6\xb8\xcb\xa3'
+p_un_sup = b'a&7\x89\xeca\x99%p!_\xef\xdd\xb9\x8f\x16'
+p_sw = b"@\xea\x88\xc0\xe9'y\xa0"
+my_key = b'\xd4\xab\x8e!\xd5\xac\xcd\xc9\x1d\x8c/\xa1U\xf8\xdf\xfe'
+my_key_e = b'u\x97f\x1b\xa8\xc7-\x99\xa9\x9cP\xad\xc4\xd6<\x11'
 
 #  GUI
--- work/tools/work_test.py
+++ work/tools/work_test.py
@@ -1,4 +1,21 @@
-from work.tools.customers import Customer
+from os import urandom
 
-cl = Customer('Orel', 'Orel-Test', '356', '172.17.237.126', '12', 'T')
-print(cl)
+from Cryptodome.Cipher import Blowfish
+# from work.tools.customers import Customer
+#
+# cl = Customer('Orel', 'Orel-Test', '356', '172.17.237.126', '12', 'T')
+# print(cl)
+
+
+#################################
+iv = b'\x88\xc3p\\\xd6\xb8\xcb\xa3'
+data = b'xxxx'
+k = b'xxxxx'
+
+cipher = Blowfish.new(k, Blowfish.MODE_CBC, iv)
+text = cipher.encrypt(data)
+print(text)
+
+# cipher = Blowfish.new(k, Blowfish.MODE_CBC, iv)
+# print(cipher.decrypt(b'\xd4\xab\x8e!\xd5\xac\xcd\xc9\x1d\x8c/\xa1U\xf8\xdf\xfe'))
+#################################
