ffcb39b564e8f8f9d1526b77a3a24eae6ab8603d
rvfedorin <35657347+rvfedorin@users.noreply.github.com>
2018-10-30 13:54:07 +0300

    Update bh/sniffer_ip_heder_decode.py

--- bh/sniffer_ip_heder_decode.py
+++ bh/sniffer_ip_heder_decode.py
@@ -1,9 +1,10 @@
 import os
 import socket
-from ctypes import *
+import struct
+from ctypes import Structure, c_ubyte, c_ushort, c_ulong
 
 # host to listen on
-host = "127.0.0.1"
+host = "192.168.40.17"
 
 
 # our ip header
@@ -13,22 +14,62 @@ class IP(Structure):
         ("version", c_ubyte, 4),
         ("tos", c_ubyte),
         ("len", c_ushort),
-        ("ihl", c_ushort),
         ("id", c_ushort),
-        ("offset", c_ubyte),
+        ("offset", c_ushort),
         ("ttl", c_ubyte),
+        ("protocol_num", c_ubyte),
         ("sum", c_ushort),
         ("src", c_ulong),
         ("dst", c_ulong),
     ]
 
-    def __init__(self):
+    def __new__(cls, socket_buffer=None):
+        return cls.from_buffer_copy(socket_buffer)
+
+    def __init__(self, socket_buffer=None):
         super().__init__()
+
+        # map protocol constants to their names
+        self.protocol_map = {1: "ICMP", 6: "TCP", 17: "UDP"}
+
+        # human readable IP addresses
+        self.src_address = socket.inet_ntoa(struct.pack("<L", self.src))
+        self.dst_address = socket.inet_ntoa(struct.pack("<L", self.dst))
+
+        # human readable protocol
+        try:
+            self.protocol = self.protocol_map[self.protocol_num]
+        except:
+            self.protocol = str(self.protocol_num)
 
 
 if os.name == "nt":
-    socket_options = socket.IPPROTO_IP
+    socket_protocol = socket.IPPROTO_IP
 else:
-    socket_options = socket.IPPROTO_ICMP
+    socket_protocol = socket.IPPROTO_ICMP
 
-sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW)
+sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+sniffer.bind((host, 0))
+sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+
+if os.name == "nt":
+    sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+
+try:
+    while True:
+
+        # read a packet
+        raw_buffer = sniffer.recvfrom(65565)[0]
+        raw_ip_header = raw_buffer[0:20]
+
+        # create an IP header from first 20 bytes of the buffer
+        ip_header = IP(raw_ip_header)
+
+        # print out the protocol that was detected and the hosts
+        print(f"Protocol: {ip_header.protocol} {ip_header.src_address} -> {ip_header.dst_address}")
+# Handle Ctrl+C
+except KeyboardInterrupt:
+    # if we are using Windwos, turn off promiscuous mode
+    if os.name == "nt":
+        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+
