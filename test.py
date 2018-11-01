5834fd25f6190ed6883bd050a404d7dc9e337e63
rvfedorin <35657347+rvfedorin@users.noreply.github.com>
2018-11-01 21:03:42 +0300

    Update test.py

--- test.py
+++ test.py
@@ -3,24 +3,46 @@
 # print(f"{192:08b}.{168:08b}.{128:08b}.{128:08b}")
 
 
-position = 3
-variable = [0, 1]
-
-temp_previous = []
-temp_last = []
-
-for p in range(position):
-    for v in variable:
-        if p > 0:
-            for back_pos in temp_previous:
-                temp_last.append(f"{back_pos}{v}")
-        else:
-            temp_last.append(f"{v}")
-    temp_previous = temp_last[:]
-    temp_last.clear()
+def combine(position:int, variable:list):
+    temp_previous = []
+    temp_last = []
+    for p in range(position):
+        for v in variable:
+            if p > 0:
+                for back_pos in temp_previous:
+                    temp_last.append(f"{back_pos}{v}")
+            else:
+                temp_last.append(f"{v}")
+        temp_previous = temp_last[:]
+        temp_last.clear()
+    return temp_previous[:]
 
 
-for i in sorted(temp_previous):
-    # print(int(i, 2))
-    print(i)
+def combine_yeld(position:int, variable:list):
+    temp_previous = []
+    temp_last = []
+    for p in range(position):
+        for v in variable:
+            if p > 0:
+                for back_pos in temp_previous:
+                    temp_last.append(f"{back_pos}{v}")
+                    if p == position-1:
+                        yield f"{back_pos}{v}"
+            else:
+                temp_last.append(f"{v}")
+        temp_previous = temp_last[:]
+        temp_last.clear()
+
+
+if __name__ == '__main__':
+
+    position = 3
+    variable = [0, 1]
+
+    all_variants = combine(position, variable)
+    all_yeld = combine_yeld(position, variable)
+
+    for i in all_yeld:
+        # print(int(i, 2))
+        print(i)
 
