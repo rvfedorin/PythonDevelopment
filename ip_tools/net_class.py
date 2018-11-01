acdfd9e10ed4994797dc41d96ce147d38f80c2f2
rvfedorin <35657347+rvfedorin@users.noreply.github.com>
2018-11-01 13:35:40 +0300

    Update ip_tools/net_class.py

--- ip_tools/net_class.py
+++ ip_tools/net_class.py
@@ -3,13 +3,18 @@ from ip_tools.ip_class import IP
 
 class NET:
     def __init__(self, net):  # net вида 192.168.0.0/24
-        self.ip, self.mask = net.split('/')
+        self.ip, self.cidr = net.split('/')
 
-    def __str__(self):
-        return f'{self.ip}/{self.mask}'
+    @property
+    def mask(self):
+        if int(self.cidr) >= 24:
+            return f"255.255.255.{256 - 2**(32-int(self.cidr))}"
 
-    def get_octet(self, oct_num=None):
-        ip_by_octets = self.ip.split('.')
+    def get_octet(self, mask=None, oct_num=None):
+        if mask:
+            ip_by_octets = self.mask.split('.')
+        else:
+            ip_by_octets = self.ip.split('.')
 
         if oct_num is None:
             return ip_by_octets
@@ -21,22 +26,20 @@ class NET:
 
     def ip_in_net(self, ip):
         net_for_ip = []
-        if ip.mask:
-            for oct in range(1, 5):
-                print(f"{int(ip.get_octet(oct))} & {int(self.get_octet(oct))}")
-                net_for_ip.append(f"{int(ip.get_octet(oct)) & int(self.get_octet(oct))}")
-            result = ".".join(net_for_ip)
-            print(result)
-            if result == self.ip:
-                return True
-            else:
-                return False
+
+        for oct in range(1, 5):
+            net_for_ip.append(f"{int(ip.get_octet(oct)) & int(self.get_octet(mask=True, oct_num=oct))}")
+        result = ".".join(net_for_ip)
+        print(result)
+        if result == self.ip:
+            return True
         else:
-            return None
+            return False
 
 
 if __name__ == '__main__':
-    ip = IP('192.168.11.2')
+    ip = IP('192.168.10.10')
     net = NET('192.168.10.0/24')
 
     print(net.ip_in_net(ip))
+    print(net.mask)
