c4af06c01685f5e59a6efde853a5860981f8448a
rvfedorin <35657347+rvfedorin@users.noreply.github.com>
2018-11-01 15:04:03 +0300

    started included_nets

--- ip_tools/net_class.py
+++ ip_tools/net_class.py
@@ -7,10 +7,16 @@ class NET:
 
     @property
     def mask(self):
-        if int(self.cidr) >= 24:
+        if int(self.cidr) < 8:
+            return f"{256 - 2**(8-int(self.cidr))}.0.0.0"
+        elif int(self.cidr) < 16:
+            return f"255.{256 - 2**(16-int(self.cidr))}.0.0"
+        elif int(self.cidr) < 24:
+            return f"255.255.{256 - 2**(24-int(self.cidr))}.0"
+        elif int(self.cidr) < 32:
             return f"255.255.255.{256 - 2**(32-int(self.cidr))}"
 
-    def get_octet(self, mask=None, oct_num=None):
+    def get_octet(self, oct_num=None, mask=None):
         if mask:
             ip_by_octets = self.mask.split('.')
         else:
@@ -36,10 +42,32 @@ class NET:
         else:
             return False
 
+    def include_nets(self, in_cidr):
+        if int(in_cidr) > int(self.cidr):
+            if int(in_cidr) < 8:
+                return f"{256 - 2**(8-int(self.cidr))}.0.0.0"
+            elif int(in_cidr) < 16:
+                return f"255.{256 - 2**(16-int(self.cidr))}.0.0"
+            elif int(in_cidr) < 24:
+                return f"255.255.{256 - 2**(24-int(self.cidr))}.0"
+            elif int(in_cidr) < 32:
+                _last = int(self.get_octet(4))
+                result = []
+                while _last <= (256 - 2 ** (32 - int(in_cidr))):
+                    _lan = f"{self.get_octet(1)}.{self.get_octet(2)}.{self.get_octet(3)}.{_last}/{in_cidr}"
+                    result.append(_lan)
+                    _last += 2 ** (32 - int(in_cidr))
+                return result
+        else:
+            return False
+
 
 if __name__ == '__main__':
     ip = IP('192.168.10.10')
     net = NET('192.168.10.0/24')
 
-    print(net.ip_in_net(ip))
-    print(net.mask)
+    # print(net.ip_in_net(ip))
+    # print(net.mask)
+
+    for i in net.include_nets(25):
+        print(i)
